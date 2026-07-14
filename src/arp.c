b0f4e696ecc5f9e44c3d913b005efa000ae90199
Ylin <3280661240@qq.com>
2026-07-15 00:00:10 +0800

    实现了arp缓存与映射关系学习 以及编写了arping程序进行测试

--- src/arp.c
+++ src/arp.c
@@ -49,12 +49,21 @@ static unsigned char local_mac[6] = {
     0x02, 0x00, 0x00, 0x00, 0x00, 0x02
 };
 
+#define ARP_CACHE_SIZE 16
+typedef struct{
+    int valid;
+    uint32_t ip;
+    unsigned char mac[ETH_ALEN];
+} arp_entry;
+static arp_entry arp_cache[ARP_CACHE_SIZE];
+
 int arp_init(){
     if(arp_initial) return 0;
     if(inet_pton(AF_INET, "10.0.0.2", &local_ip) != 1){
-        perror("ERR: Could not initialize ARP IP");
+        fprintf(stderr,"ERR: Could not initialize ARP IP\n");
         return -1;
     }
+    memset(arp_cache, 0, sizeof(arp_cache));
     arp_initial = 1;
     return 0;
 }
@@ -64,7 +73,7 @@ static int arp_reply(const arp_pkt *packet){
 
     memset(&reply, 0, sizeof(reply));
     // 以太网帧头部
-    memcpy(reply.eth.dst, packet->arp.smac,ETH_ALEN);
+    memcpy(reply.eth.dst, packet->eth.src,ETH_ALEN);
     memcpy(reply.eth.src, local_mac,ETH_ALEN);
     reply.eth.type = htons(ETH_P_ARP);
     // ARP头部
@@ -83,6 +92,46 @@ static int arp_reply(const arp_pkt *packet){
         perror("ERR: Could not write ARP reply");
         return -1;
     }
+    return 0;
+}
+
+static int arp_cache_find(uint32_t ip){
+    int i;
+    for(i=0; i<ARP_CACHE_SIZE; i++){
+        if(arp_cache[i].valid && (arp_cache[i].ip == ip))
+            return i;
+    }
+    return -1;
+}
+
+static int arp_cache_update(uint32_t ip, const unsigned char *mac){
+    int i;
+    int index;
+    index = arp_cache_find(ip);
+    if(index >= 0){
+        memcpy(arp_cache[index].mac, mac, ETH_ALEN);
+        return 0;
+    }
+    for(i=0; i<ARP_CACHE_SIZE; i++){
+        if(!arp_cache[i].valid){
+            arp_cache[i].valid = 1;
+            arp_cache[i].ip = ip;
+            memcpy(arp_cache[i].mac, mac, ETH_ALEN);
+            return 0;
+        }
+    }
+    arp_cache[0].valid = 1;
+    arp_cache[0].ip = ip;
+    memcpy(arp_cache[0].mac, mac, ETH_ALEN); 
+    return 0;
+}
+
+int arp_lookup(uint32_t ip, unsigned char *mac){
+    int index;
+    if(mac == NULL) return -1;
+    index = arp_cache_find(ip);
+    if(index < 0) return -1;
+    memcpy(mac, arp_cache[index].mac, ETH_ALEN);
     return 0;
 }
 
@@ -133,16 +182,19 @@ int arp_handle(void *buf, int len){
     opcode = ntohs(packet->arp.opcode);
     if(opcode == ARP_REQUEST){
         // 如果REQUEST请求不是本机的IP 则忽略
+        arp_cache_update(packet->arp.sip, packet->arp.smac);
         if(packet->arp.tip != local_ip) return 0;
         return arp_reply(packet);
     }else if(opcode == ARP_REPLY){
         // 从packet中学习发送IP和发送mac的映射关系
+        arp_cache_update(packet->arp.sip, packet->arp.smac);
         return 0;
     }
     return 0;
 }
 
 int arp_free(){
+    memset(arp_cache, 0, sizeof(arp_cache));
     arp_initial = 0;
     local_ip = 0;
     return 0;
--- src/main.c
+++ src/main.c
@@ -1,36 +1,111 @@
+#include <arpa/inet.h>
+#include <linux/if_ether.h>
 #include <stdio.h>
 
 #include "arp.h"
 #include "tun.h"
 
+static void print_mac(const unsigned char *mac)
+{
+    printf(
+        "%02x:%02x:%02x:%02x:%02x:%02x]\n",
+        mac[0],
+        mac[1],
+        mac[2],
+        mac[3],
+        mac[4],
+        mac[5]
+    );
+}
+
 int main()
 {
     unsigned char buf[2048];
+    unsigned char mac[ETH_ALEN];
+
+    struct in_addr target_ip;
+
     int len;
 
+    /*
+     * 创建 tap0。
+     */
     if (tun_init() < 0) {
-        fprintf(stderr, "Could not initialize TAP device\n");
+        fprintf(stderr, "ERR: Could not initialize TAP\n");
         return -1;
     }
 
+    /*
+     * 初始化用户态 ARP：
+     *
+     * IP  = 10.0.0.2
+     * MAC = 02:00:00:00:00:02
+     */
     if (arp_init() < 0) {
-        fprintf(stderr, "Could not initialize ARP\n");
+        fprintf(stderr, "ERR: Could not initialize ARP\n");
         tun_free();
         return -1;
     }
 
-    printf("ARP program started\n");
+    /*
+     * 查询 Linux tap0 的 IP。
+     */
+    if (inet_pton(AF_INET, "10.0.0.1", &target_ip) != 1) {
+        fprintf(stderr, "ERR: Invalid target IP\n");
 
+        arp_free();
+        tun_free();
+
+        return -1;
+    }
+    printf("Configure tap0, then press Enter...\n");
+    getchar();
+    
+    printf("ARPING 10.0.0.1\n");
+
+    /*
+     * 发送：
+     *
+     * Who has 10.0.0.1?
+     * Tell 10.0.0.2
+     */
+    if (arp_request(target_ip.s_addr) < 0) {
+        fprintf(stderr, "ERR: Could not send ARP request\n");
+
+        arp_free();
+        tun_free();
+
+        return -1;
+    }
+
+    /*
+     * 等待 Linux 返回 ARP Reply。
+     */
     while (1) {
         len = tun_read(buf, sizeof(buf));
 
         if (len < 0) {
-            perror("tun_read");
+            perror("ERR: Could not read TAP");
             break;
         }
 
+        /*
+         * 如果收到的是 ARP Reply，
+         * arp_handle() 会更新 ARP 缓存。
+         */
         if (arp_handle(buf, len) < 0) {
-            fprintf(stderr, "arp_handle failed\n");
+            fprintf(stderr, "ERR: Could not handle ARP packet\n");
+            break;
+        }
+
+        /*
+         * 每处理完一个数据包，都检查目标地址
+         * 是否已经进入 ARP 缓存。
+         */
+        if (arp_lookup(target_ip.s_addr, mac) == 0) {
+            printf("Reply from 10.0.0.1 [");
+            print_mac(mac);
+
             break;
         }
     }
